3a0e1dc3cf1efcb1530dfd7750405cd63397ca6c
takacszsolt2022d-lab <takacs.zsolt.2022@telekimezotur.hu>
2026-09-17 09:44:48 +0200

    kaveseleje

---
+++ KavezogepAp/kavezogep.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KavezogepAp
+{
+    internal class kavezogep
+    {
+        private int Vizkeszlet;
+
+        public void VizTolt(int ml)
+        {
+            if (ml <0)
+            {
+                Vizkeszlet = 0;
+            }
+            else Vizkeszlet += ml;
+        }
+        public void KaveFozes()
+        {
+            if(Vizkeszlet >= 150)
+            {
+                Vizkeszlet -= 150;
+            }
+            else
+            {
+                Console.WriteLine("Nem áll elég víz rendelkezésre a főzéshez.");
+            }
+        }
+    }
+}
