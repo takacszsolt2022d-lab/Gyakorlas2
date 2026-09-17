2af4ae2e8488b893794ac74273316d5140be65f1
takacszsolt2022d-lab <takacs.zsolt.2022@telekimezotur.hu>
2026-09-17 09:36:27 +0200

    overrideos

---
+++ DiakToString/Diak.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiakApp
+{
+    internal class Diak
+    {
+        public string Nev { get; set; }
+
+        public double Atlag { get; set; }
+
+        public Diak()
+        {
+            Nev = "Molnar Marcell";
+            Atlag = 4.5;
+        }
+
+        public Diak( string kapottnev, double kapottatlag)
+        {
+            Nev = kapottnev;
+            Atlag = kapottatlag;
+
+        }
+
+        public Diak(string csaknev)
+        {
+            Nev = csaknev;
+            Atlag = 4.2;
+        }
+
+        public override string ToString()
+        {
+            return $"Név: {Nev}, Átlag: {Atlag}";
+        }
+    }
+}
---
+++ DiakToString/Program.cs
@@ -0,0 +1,8 @@
+﻿using DiakApp;
+
+Diak diak1 = new Diak();
+Diak diak2 = new Diak("Nagy Anna", 2.9);
+Diak diak3 = new Diak("Kovvács Misi");
+Console.WriteLine(diak1);
+Console.WriteLine(diak2);
+Console.WriteLine(diak3);
